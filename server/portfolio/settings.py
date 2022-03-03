df410d42aca6b3b92a92d80c0b1eb75bd756cf73
Guillaume Kinif <guillaume.kinif@collibra.com>
2022-03-03 21:55:03 +0100

    add custom user to login using email

--- server/portfolio/settings.py
+++ server/portfolio/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+
+    'accounts.apps.AccountsConfig',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # Internationalization
